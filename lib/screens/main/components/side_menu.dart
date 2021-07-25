215ae68baf9dbb562128b6bdad955f494ce12b96
CharwekarAkshay <charwekar12@gmail.com>
2021-07-25 22:52:28 +0530

    Created social media

--- lib/screens/main/components/side_menu.dart
+++ lib/screens/main/components/side_menu.dart
@@ -45,6 +45,47 @@ class SideMenu extends StatelessWidget {
                   Coding(),
                   SizedBox(height: defaultPadding),
                   Knowledges(),
+                  Divider(),
+                  SizedBox(height: defaultPadding),
+                  TextButton(
+                    onPressed: () {},
+                    child: FittedBox(
+                      child: Row(
+                        children: [
+                          Text(
+                            "DONWLOAD CV",
+                            style: TextStyle(
+                              color:
+                                  Theme.of(context).textTheme.bodyText1!.color,
+                            ),
+                          ),
+                          SizedBox(width: defaultPadding / 2),
+                          SvgPicture.asset("assets/icons/download.svg"),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: defaultPadding),
+                    color: Color(0xFF24242E),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset("assets/icons/linkedin.svg"),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset("assets/icons/github.svg"),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset("assets/icons/twitter.svg"),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
